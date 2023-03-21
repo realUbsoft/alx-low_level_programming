e859a77783735f9550078f00739d06da4afbbce8
realUbsoft <swaggersoft@gmail.com>
2023-03-20 19:29:09 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,12 @@
 
 /**
 
-* main - Entry point
+* main - prints " with proper grammar, but the outcome is 
+* a piece of art," followed by a new line.
 
 *
 
-* Return: Always 0 (Success)
+* Return: Always 0,
 
 */
 
