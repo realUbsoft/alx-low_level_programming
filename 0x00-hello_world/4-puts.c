13b2082143554139f1968697e34e6caf618994ff
realUbsoft <swaggersoft@gmail.com>
2023-03-20 19:49:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
-
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-        puts("\nProgramming is like building a multilingual puzzle.");
-
-        return (0);
-
+	return (0);
 }
